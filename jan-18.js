e8eab7d1e3093ee1d565b0291bb16e7be7982e74
Prithiviraj Ramesh <96234646+Prithiv11@users.noreply.github.com>
2022-01-22 19:26:06 +0530

    Add files via upload

---
+++ jan-18.js
@@ -0,0 +1,47 @@
+var data = [
+        {
+             "id": 1,
+             "title": "Title 1",
+             "childrens": [
+                  {
+                       "id": 2,
+                       "title": "Title 2",
+                       "childrens": []
+                  }
+             ]
+        },
+        {
+             "id": 3,
+             "title": "Title 3",
+             "childrens": [
+                  {
+                       "id": 4,
+                       "title": "Title 4",
+                       "childrens": [
+                            {
+                                 "id": 5,
+                                 "title": "Title 5",
+                                 "childrens": []
+                            }
+                       ]
+                  }
+             ]
+        }
+    ]
+    var output = [];
+
+    function unpack(obj){
+        let tempObj={}
+        tempObj.id=obj.id;
+        tempObj.title=obj.title;
+        tempObj.childrens=[];
+        output.push(tempObj);
+        obj.childrens.forEach(el => {
+            unpack(el);
+        });
+        console.log(output);
+    }
+
+    data.forEach(element=>{
+        unpack(element);
+    })
